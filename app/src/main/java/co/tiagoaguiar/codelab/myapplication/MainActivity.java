4a0e87e889cfbb16e0595f37d863f7e2c6605770
João Victor Agapito <55551801+JoaoAgapito@users.noreply.github.com>
2022-05-24 07:42:18 -0300

    2.0

--- app/src/main/java/co/tiagoaguiar/codelab/myapplication/MainActivity.java
+++ app/src/main/java/co/tiagoaguiar/codelab/myapplication/MainActivity.java
@@ -1,28 +1,80 @@
 package co.tiagoaguiar.codelab.myapplication;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView rvMain;
 
-	private View btnImc;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+        rvMain = findViewById(R.id.main_rv);
 
-		btnImc = findViewById(R.id.btn_imc);
+        List<MainItem> mainItems = new ArrayList<>();
+        mainItems.add(new MainItem(1, R.drawable.ic_baseline_wb_sunny_24, R.string.label_imc, Color.GREEN));
+        mainItems.add(new MainItem(2, R.drawable.ic_baseline_visibility_24, R.string.label_tmb, Color.BLUE));
 
-		// codigo para abrir uma nova tela depois do evento de click.
 
-		btnImc.setOnClickListener(view -> {
-			Intent intent = new Intent(MainActivity.this, imcActivity.class);
-			startActivity(intent);
-		});
-	}
+        rvMain.setLayoutManager(new LinearLayoutManager(this));
+        MainAdapter adapter = new MainAdapter(mainItems);
+        rvMain.setAdapter(adapter);
+
+    }
+    private class MainAdapter extends RecyclerView.Adapter<MainViewHolder>{
+        private List<MainItem> mainItems;
+        public MainAdapter(List<MainItem> mainItems) {
+            this.mainItems = mainItems;
+        }
+
+        @NonNull
+        @Override
+        public MainViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new MainViewHolder(getLayoutInflater().inflate(R.layout.main_item, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MainViewHolder holder, int position) {
+          MainItem mainItemCurrent = mainItems.get(position);
+            holder.bind(mainItemCurrent);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mainItems.size();
+        }
+    }
+    private class MainViewHolder extends RecyclerView.ViewHolder{
+
+        public MainViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+        public void bind(MainItem item) {
+            TextView txtName = itemView.findViewById(R.id.item_txt_name);
+            ImageView imgIcon  = itemView.findViewById(R.id.item_img_icon);
+            LinearLayout container = (LinearLayout) itemView;
+
+            txtName.setText(item.getTextStringID());
+            imgIcon.setImageResource(item.getDrawableID());
+            container.setBackgroundColor(item.getColor());
+
+        }
+    }
 }
